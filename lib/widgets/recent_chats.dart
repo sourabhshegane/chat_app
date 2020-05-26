61c064098b4c095ae5c2aba33d11f381278eb69d
Sourabh Shegane <sgsourabh30@gmail.com>
2020-05-26 20:48:04 +0530

    Added Code to dislay read and unread messages

--- lib/widgets/recent_chats.dart
+++ lib/widgets/recent_chats.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
+
 import 'package:chatapp/services/message.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class RecentChats extends StatefulWidget {
   @override
@@ -16,12 +19,88 @@ class _RecentChatsState extends State<RecentChats> {
             borderRadius: BorderRadius.only(
                 topLeft: Radius.circular(30.0),
                 topRight: Radius.circular(30.0))),
-        child: ListView.builder(
-            itemCount: chats.length, itemBuilder: (BuildContext context, int index){
-          return Text(
-              chats[index].sender.name
-          );
-        }),
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30.0),
+              topRight: Radius.circular(30.0)),
+          child: ListView.builder(
+              scrollDirection: Axis.vertical,
+              itemCount: chats.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  margin: EdgeInsets.only(top: 5.0, bottom: 5.0, right: 20.0),
+                  padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                  decoration: BoxDecoration(
+                    color: chats[index].unread ? Color(0xFFFFEFEE) : Colors.white,
+                  ),
+                  child: Row(
+                    children: [
+                      Row(
+                        children: [
+                          Row(
+                            children: [
+                              CircleAvatar(
+                                radius: 35.0,
+                                backgroundImage:
+                                    AssetImage(chats[index].sender.imageUrl),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width*0.48,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      chats[index].sender.name,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(fontSize: 14),
+                                    ),
+                                    Text(
+                                      chats[index].text,
+                                      overflow: TextOverflow.ellipsis,
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          Container(
+                            child: Column(
+                              children: [
+                                Text(
+                                  chats[index].time,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                chats[index].unread ? Container(
+                                  width: 50,
+                                  height: 20,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(30),
+                                    color: Theme.of(context).primaryColor
+                                  ),
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    "NEW",
+                                    style: TextStyle(
+                                      color: Colors.white
+                                    ),
+                                  ),
+                                ) : SizedBox.shrink(),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              }),
+        ),
       ),
     );
   }
